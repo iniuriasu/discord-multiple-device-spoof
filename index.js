495e8271b12a4ac199464f36a16027694752f5b4
iniuriasu <161624491+iniuriasu@users.noreply.github.com>
2024-07-11 14:18:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
 const { Client } = require('discord.js-selfbot-v13');
-
 const platforms = {
     web: {
-        userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         properties: { $browser: 'Discord Web' }
     },
     phone: {
-        userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
         properties: { $browser: 'Discord iOS' }
     },
-    pc: {
-        userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        properties: { $os: 'Windows', $browser: 'Discord PC' }
+    console: {
+        properties: { $os: 'Windows', $browser: 'Discord Embedded' }
     }
 };
 
 const clients = [];
 
-Object.entries(platforms).forEach(([platform, { userAgent, properties }]) => {
+Object.entries(platforms).forEach(([ { properties }]) => {
     const client = new Client({
-        ws: { properties },
-        http: { userAgent }
+        ws: { properties }
     });
-
-    client.on('ready', () => {
-        console.log(`Logged in as ${client.user.tag} on ${platform}`);
-    });
-
     clients.push(client);
 });
 
-
 clients.forEach(client => {
-    client.login('token');
+    client.login('TOKEN').catch(console.error);
 });
+
+/*
+
+Release 1.1
+
+Deleted useless lines,
+thanks to my friend added "console" spoof.
+
+
+*/
